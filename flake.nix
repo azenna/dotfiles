af853facfbe4cf308817fa3120a5d8d64ee29bfa
azenna <zrallwein@gmail.com>
2024-02-26 15:54:45 -0600

    trying to fix things

--- flake.nix
+++ flake.nix
@@ -47,7 +47,6 @@
 
             services.xserver.displayManager.gdm.enable = true;
             services.xserver.desktopManager.gnome.enable = true;
-            services.gnome.core-utilities.enable = false;
 
             services.xserver = {
               xkb.layout = "us";
