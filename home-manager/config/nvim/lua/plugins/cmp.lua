aee4c113e1bd7df0a221461007201c126d1168c6
azenna <zrallwein@gmail.com>
2024-03-02 23:43:19 -0600

    luasnip

--- home-manager/config/nvim/lua/plugins/cmp.lua
+++ home-manager/config/nvim/lua/plugins/cmp.lua
@@ -1,37 +1,35 @@
 return {
-  "hrsh7th/nvim-cmp"
-  , dependencies = {
-      "hrsh7th/cmp-nvim-lsp"
-    , "hrsh7th/cmp-nvim-lua"
-    , "hrsh7th/cmp-buffer"
-    , "hrsh7th/cmp-path"
-    , "hrsh7th/cmp-cmdline"
-    , "saadparwaiz1/cmp_luasnip"
-    , "L3MON4D3/LuaSnip" 
-  },
-  config = function()
-    local cmp = require("cmp")
-    require("cmp").setup({
-      snippet = {
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      }),
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "nvim_lua" },
-        { name = "luasnip" },
-      }, {
-        { name = "buffer" },
-        { name = "path" },
-      }),
-    })
-  end,
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lua",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"saadparwaiz1/cmp_luasnip",
+		"L3MON4D3/LuaSnip",
+	},
+	config = function()
+		local cmp = require("cmp")
+		require("cmp").setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to
+				["<C-f>"] = cmp.mapping.select_next_item({ behavior = types.cmp.SelectBehavior.Insert }),
+				["<C-b>"] = cmp.mapping.select_prev_item({ behavior = types.cmp.SelectBehavior.Insert }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "nvim_lua" },
+				{ name = "luasnip" },
+			}, {
+				{ name = "buffer" },
+				{ name = "path" },
+			}),
+		})
+	end,
 }
