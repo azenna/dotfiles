ec73fca7c5cec1ea4591844dcb5552dbd690f9c3
azenna <zrallwein@gmail.com>
2024-03-06 23:13:10 -0600

    205   2024-03-06 23:13:09   (current)

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -163,7 +163,18 @@
     };
 
     bashrcExtra = ''
-      eval "$(starship init bash)"
+      if [[ -z "$ZELLIJ" ]]; then
+          if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
+              zellij attach -c
+          else
+              zellij
+          fi
+
+          if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
+            exit
+          fi
+      fi
+      set -o vi
       eval "$(atuin init bash)"
     '';
   };
