e41272c87d39e7823cf5af51f36b2a1cbb35fd9f
azenna <zrallwein@gmail.com>
2024-03-15 02:58:58 -0500

    289   2024-03-15 02:58:56   (current)

--- home-manager/scripts/eww/workspaces.nix
+++ home-manager/scripts/eww/workspaces.nix
@@ -2,7 +2,7 @@
 pkgs.writeShellScriptBin "eww_workspaces" ''
   spaces (){
   	WORKSPACE_WINDOWS=$(hyprctl workspaces -j | jq 'map({key: .id | tostring, value: .windows}) | from_entries')
-  	seq 1 10 | jq --argjson windows "$${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., windows: ($windows[.]//0)})'
+  	seq 1 10 | jq --argjson windows "$WORKSPACE_WINDOWS" --slurp -Mc 'map(tostring) | map({id: ., windows: ($windows[.]//0)})'
   }
 
   spaces
