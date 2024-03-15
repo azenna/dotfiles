997406b58436225fb274050a4c880cadad2fc1c9
azenna <zrallwein@gmail.com>
2024-03-15 04:31:04 -0500

    297   2024-03-15 04:31:03   (current)

--- home-manager/common/starship/default.nix
+++ home-manager/common/starship/default.nix
@@ -1,11 +1,9 @@
 {pkgs, ...}: {
-  home.file.".config/starship/starship.toml" = {
+  home.file.".config/starship.toml" = {
     source = ./starship.toml;
   };
   # starship - an customizable prompt for any shell
   programs.starship = {
     enable = true;
-    # custom settings
-    settings = {add_newline = false;};
   };
 }
