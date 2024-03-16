71354d8006f922e0feb763691c45ed4881323239
azenna <zrallwein@gmail.com>
2024-03-16 08:05:51 -0500

    304   2024-03-16 08:05:49   (current)

--- home-manager/hyprland/eww/default.nix
+++ home-manager/hyprland/eww/default.nix
@@ -8,5 +8,6 @@
     eww-wayland
     (import ./scripts/active_workspace.nix {inherit pkgs;})
     (import ./scripts/workspaces.nix {inherit pkgs;})
+    (import ./scripts/network.nix {inherit pkgs;})
   ];
 }
