7033cc96731c5720c002ce78ea9c351fb91b7bbe
azenna <zrallwein@gmail.com>
2024-03-18 11:01:07 -0500

    320   2024-03-18 11:01:05   (current)

--- systems/ephemerate/configuration.nix
+++ systems/ephemerate/configuration.nix
@@ -6,6 +6,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./cachix.nix
   ];
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
