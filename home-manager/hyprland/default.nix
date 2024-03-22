ebeabb47069a54592e1c0499b89f9aaf07b289cc
azenna <zrallwein@gmail.com>
2024-03-22 13:20:42 -0500

    327   2024-03-22 13:19:39   (current)

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -1,5 +1,12 @@
 {pkgs, ...}: {
   imports = [../common ./hypr ./rofi ./eww];
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Materia-dark";
+      package = pkgs.materia-theme;
+    };
+  };
   home.packages = with pkgs; [
     copyq
     grim
